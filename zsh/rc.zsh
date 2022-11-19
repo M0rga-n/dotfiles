28d79d03edc9973469e77a4afef5065df0387894
M0rga-n <verburgnoah@gmail.com>
2022-11-19 19:35:41 +0100

    zsh

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -4,4 +4,17 @@ source_if_exists () {
 	fi
 }
 
+source_if_exists $HOME/.env.sh
+source_if_exists $DOTFILES/zsh/history.zsh
+source_if_exists $DOTFILES/zsh/aliases.zsh
+
+autoload -U zmv
+autoload -U promptinit && promptinit
+autoload _U colors && colors
+autoload _Uz compinit && compinit
+
+precmd() {
+	source $DOTFILES/zsh/aliases.zsh
+}
+
 eval "$(starship init zsh)""
